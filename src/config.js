6be703e235985a03e4549732857bb6fccc313c9b
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-01-20 21:11:11 -0500

    deploy ready

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://sleepy-inlet-36289.herokuapp.com',
+  API_ENDPOINT: 'https://boiling-gorge-62514.herokuapp.com',
   TOKEN_KEY: "blogful-client-auth-token",
 }
